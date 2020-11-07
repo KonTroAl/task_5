e25db56bb40821abc0a166539167c8a2554baec1
KonTroAl <troshenkin.k@yandex.ru>
2020-11-07 12:45:19 +0500

    completed task 2

--- task_2.py
+++ task_2.py
@@ -11,3 +11,35 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
+from collections import defaultdict
+from functools import reduce
+
+my_dict = defaultdict(list)
+print(my_dict)
+
+a = list("A2")
+for i in range(len(a)):
+    my_dict[1].append(a[i])
+
+b = list("C4F")
+for i in range(len(b)):
+    my_dict[2].append(b[i])
+
+print(my_dict)
+
+my_list = []
+my_list.append(int("".join(my_dict[1]), 16))
+my_list.append(int("".join(my_dict[2]), 16))
+
+my_sum = list(hex(reduce(lambda x, y: x + y, my_list))[2:].upper())
+for i in range(len(my_sum)):
+    my_dict["sum"].append(my_sum[i])
+
+my_mul = list(hex(reduce(lambda x, y: x * y, my_list))[2:].upper())
+for i in range(len(my_mul)):
+    my_dict["mul"].append(my_mul[i])
+
+print(my_dict)
+
+# print(f"Сумма чисел {my_dict[1]} и {my_dict[2]} равна {my_sum}")
+# print(f"Произведение чисел {my_dict[1]} и {my_dict[2]} равна {my_mul}")
